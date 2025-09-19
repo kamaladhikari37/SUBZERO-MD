c7582d0be4093161673997ad12fb703838d329ed
kamaladhikari37 <ka6037268@gmail.com>
2025-09-20 01:58:34 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;zc1GUSYR#yX_O9ql83g0m_r3QKbAnfTmPWjBRyVui7ofkK4rqC2I", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "917499190619", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
